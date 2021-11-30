de63d5748589599f5a4c9a0cb11b3a29a264a27c
Evgenia Bazelan <evgeniabazelan@gmail.com>
2021-11-30 22:24:21 +0300

    nav

--- movies-explorer/src/components/Navigation/Navigation.js
+++ movies-explorer/src/components/Navigation/Navigation.js
@@ -6,16 +6,14 @@ import "./_active/navigation__menu-toggle_active.css"
 import accauntLogo from "../../images/icon-accaunt.svg";
 import { useState } from "react";
 
-const Navigation = () => {
+const Navigation = ({ isLogin }) => {
+  const [clickMenuBurger, setClickMenuBurger] = useState(false);
+  const { pathname } = useLocation();
 
-    const [clickMenuBurger, setClickMenuBurger] = useState(false);
-    const { pathname } = useLocation();
-
-    const accauntLinksHidden = `${pathname === "/" ? "navigation_hidden" : ""}`;
-
-    const navLinksHidden = `${pathname === "/sign-in" || pathname === "/sign-up"
-        ? "navigation_hidden"
-        : ""
+  const navLinksHidden = `${pathname === "/sign-in"
+    || pathname === "/sign-up"
+    ? "navigation_hidden"
+    : ""
     }`;
 
     const authLinksHidden = `${pathname === "/movies" ||
@@ -25,183 +23,107 @@ const Navigation = () => {
         : ""
     }`;
 
+  const handleClickMenuBurger = () => {
+      setClickMenuBurger(!clickMenuBurger)
+  };
 
 
-    const handleClickMenuBurger = () => {
-        if (clickMenuBurger) {
-            setClickMenuBurger(false);
-        } else {
-            setClickMenuBurger(true);
-        }
-    };
+  return (
+    <nav className={`navigation ${navLinksHidden}`}>
+      <div
+        className={`navigation__burger-button ${!isLogin && "navigation_hidden"
+          }
+      `}
+      >
+        <input
+          className={`navigation__menu-toggle
+         `}
+          id="navigation__menu-toggle"
+          type="checkbox"
+        />
+        <label
+          onClick={handleClickMenuBurger}
+          className="navigation__menu-btn"
+          htmlFor="navigation__menu-toggle"
+        >
+          <span></span>
+        </label>
+      </div>
 
-    const headerNavigateWidth = `${pathname === "/sign-in"
-    || pathname === "/sign-up" || pathname === "/movies" || pathname === "/saved-movies"
-    || pathname === "/profile"
-        ? "navigation_width"
-        : ""}`
+      {/*<ul*/}
+      {/*  className={`navigation__links*/}
+      {/*${isLogin && "navigation_hidden"}`}*/}
+      {/*>*/}
+        <ul className={`navigation__links ${authLinksHidden}`}>
+        <li className="navigation__links-item ">
+          <Link className="navigation__register-link" to="/sign-up">
+            Регистрация
+          </Link>
+        </li>
+        <li className="navigation__links-item">
+          <Link className="navigation__login-link" to="/sign-in">
+            Войти
+          </Link>
+        </li>
+      </ul>
 
-    return (
-        <nav className={`navigation ${navLinksHidden} ${headerNavigateWidth}`}>
-
-            <div className={`navigation__burger-button ${accauntLinksHidden} ${clickMenuBurger ? "navigation_fixed" : ""}`}>
-                <input id="navigation__menu-toggle" type="checkbox" />
-                <label
-                    onClick={handleClickMenuBurger}
-                    className="navigation__menu-btn"
-                    for="navigation__menu-toggle">
-                    <span></span>
-                </label>
-            </div>
-
-            <ul className={`navigation__links ${authLinksHidden}`}>
-                <li className="navigation__links-item ">
-                    <Link className="navigation__register-link" to="/sign-up">
-                        Регистрация
-                    </Link>
-                </li>
-                <li className="navigation__links-item">
-                    <Link className="navigation__login-link" to="/sign-in">
-                        Войти
-                    </Link>
-                </li>
-            </ul>
-            <div className={`navigation__sublayer ${!clickMenuBurger ? "navigation_hidden" : " "}`}></div>
-            <ul className={`navigation__links navigation__links_column navigation__links_visibility_off ${accauntLinksHidden} ${clickMenuBurger ? "navigation_opened" : ""}`}>
-                <li className="navigation__links-item navigation_hidden-link navigation__links-item_margin_null">
-                    <Link className="navigation__main-link" to="/">
-                        Главная
-                    </Link>
-                </li>
-                <li className="navigation__links-item navigation__links-item_direction_column navigation__links-item_margin_null">
-                    <Link className="navigation__movies-link" to="/movies">
-                        Фильмы
-                    </Link>
-                    <Link className="navigation__save-films-link" to="/saved-movies">
-                        Сохранённые фильмы
-                    </Link>
-                </li>
-                <li className="navigation__links-item">
-                    <Link className="navigation__profile-link" to="/profile">
-                        Аккаунт
-                        <img className="navigation__accaunt-image" src={accauntLogo} alt="Логотип аккаунта"></img>
-                    </Link>
-                </li>
-            </ul>
-
-        </nav >
-    );
+      <div
+        className={`navigation__sublayer
+      ${!clickMenuBurger && "navigation_hidden"}`}
+      ></div>
+      <ul
+        className={`navigation__links
+      navigation__links_column
+      navigation__links_visibility_off
+     ${!isLogin && "navigation_hidden"}
+      ${clickMenuBurger && "navigation_opened"}`}
+      >
+        <li
+          className="navigation__links-item
+        navigation_hidden-link
+        navigation__links-item_margin_null"
+        >
+          <Link
+            className="navigation__main-link"
+            to="/"
+            onClick={handleClickLink}
+          >
+            Главная
+          </Link>
+        </li>
+        <li className="navigation__links-item navigation__links-item_direction_column navigation__links-item_margin_null">
+          <Link
+            className="navigation__movies-link "
+            to="/movies"
+            onClick={handleClickMenuBurger}
+          >
+            Фильмы
+          </Link>
+          <Link
+            className="navigation__save-films-link"
+            to="/saved-movies"
+            onClick={handleClickMenuBurger}
+          >
+            Сохранённые фильмы
+          </Link>
+        </li>
+        <li className="navigation__links-item">
+          <Link
+            className="navigation__profile-link"
+            to="/profile"
+            onClick={handleClickMenuBurger}
+          >
+            Аккаунт
+            <img
+              className="navigation__accaunt-image"
+              src={accauntLogo}
+              alt="Логотип аккаунта"
+            />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
-// const Navigation = ({ isLogin }) => {
-//   const [clickMenuBurger, setClickMenuBurger] = useState(false);
-//   const { pathname } = useLocation();
-//
-//   const navLinksHidden = `${pathname === "/sign-in"
-//     || pathname === "/sign-up"
-//     ? "navigation_hidden"
-//     : ""
-//     }`;
-//
-//   const handleClickMenuBurger = () => {
-//       setClickMenuBurger(!clickMenuBurger)
-//   };
-//
-//
-//   return (
-//     <nav className={`navigation ${navLinksHidden}`}>
-//       <div
-//         className={`navigation__burger-button ${!isLogin && "navigation_hidden"
-//           }
-//       `}
-//       >
-//         <input
-//           className={`navigation__menu-toggle
-//          `}
-//           id="navigation__menu-toggle"
-//           type="checkbox"
-//         />
-//         <label
-//           onClick={handleClickMenuBurger}
-//           className="navigation__menu-btn"
-//           htmlFor="navigation__menu-toggle"
-//         >
-//           <span></span>
-//         </label>
-//       </div>
-//
-//       <ul
-//         className={`navigation__links
-//       ${isLogin && "navigation_hidden"}`}
-//       >
-//         <li className="navigation__links-item ">
-//           <Link className="navigation__register-link" to="/sign-up">
-//             Регистрация
-//           </Link>
-//         </li>
-//         <li className="navigation__links-item">
-//           <Link className="navigation__login-link" to="/sign-in">
-//             Войти
-//           </Link>
-//         </li>
-//       </ul>
-//
-//       <div
-//         className={`navigation__sublayer
-//       ${!clickMenuBurger && "navigation_hidden"}`}
-//       ></div>
-//       <ul
-//         className={`navigation__links
-//       navigation__links_column
-//       navigation__links_visibility_off
-//      ${!isLogin && "navigation_hidden"}
-//       ${clickMenuBurger && "navigation_opened"}`}
-//       >
-//         <li
-//           className="navigation__links-item
-//         navigation_hidden-link
-//         navigation__links-item_margin_null"
-//         >
-//           <Link
-//             className="navigation__main-link"
-//             to="/"
-//             // onClick={handleClickLink}
-//           >
-//             Главная
-//           </Link>
-//         </li>
-//         <li className="navigation__links-item navigation__links-item_direction_column navigation__links-item_margin_null">
-//           <Link
-//             className="navigation__movies-link "
-//             to="/movies"
-//             // onClick={handleClickLink}
-//           >
-//             Фильмы
-//           </Link>
-//           <Link
-//             className="navigation__save-films-link"
-//             to="/saved-movies"
-//             // onClick={handleClickLink}
-//           >
-//             Сохранённые фильмы
-//           </Link>
-//         </li>
-//         <li className="navigation__links-item">
-//           <Link
-//             className="navigation__profile-link"
-//             to="/profile"
-//             // onClick={handleClickLink}
-//           >
-//             Аккаунт
-//             <img
-//               className="navigation__accaunt-image"
-//               src={accauntLogo}
-//               alt="Логотип аккаунта"
-//             />
-//           </Link>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// };
 
 export default Navigation;

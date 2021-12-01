5f887b95b827aaa2e6f4435484bae9da57715cc0
Evgenia Bazelan <evgeniabazelan@gmail.com>
2021-12-01 22:38:24 +0300

    history push pathname comment

--- movies-explorer/src/components/App/App.js
+++ movies-explorer/src/components/App/App.js
@@ -49,7 +49,9 @@ const App = () => {
                 mainApi.setItemToken(jwt);
                 setLoggedIn(true);
                 setIsLoading(false);
-                history.push(pathname);
+                if (pathname !== "/sign-in" && pathname !== "/sign-up" ) {
+                    history.push(pathname);
+                }
             })
             .catch(err => {
                 setLoggedIn(false);
